637a5bce95a0a7df92987978b5b595c208613d9b
Lujun LI <lilujun588588@gmail.com>
2024-12-12 18:37:45 +0100

    update code

--- training.py
+++ training.py
@@ -1,12 +1,14 @@
 # ====================================================IMPORT=================================================================
 import os
+import warnings
 
+warnings.filterwarnings("ignore")
 os.environ.update(
     {
-        "NCCL_P2P_DISABLE": "1",
-        "NCCL_IB_DISABLE": "1",
-        "TOKENIZERS_PARALLELISM": "false",
-        "CUDA_VISIBLE_DEVICES": "3,2,1,0",
+        # "NCCL_P2P_DISABLE": "1",
+        # "NCCL_IB_DISABLE": "1",
+        # "TOKENIZERS_PARALLELISM": "false",
+        # "CUDA_VISIBLE_DEVICES": "3,2,1",
     }
 )
 
@@ -46,98 +48,29 @@ from accelerate import Accelerator
 # ====================================================SETTINGS=================================================================
 current = time.time()
 output_dir = f"logs/fit_{current}"
-
 resume_from_checkpoint = False
 # output_dir = "/home/llama/Personal_Directories/srb/mt_luxembourgish/logs/fit_1733825724.5355668"
 
-learning_rate = 1e-4
-per_device_train_batch_size = 2
-per_device_eval_batch_size = 2
+learning_rate = 2e-5
+per_device_train_batch_size = 10
+per_device_eval_batch_size = 10
 num_train_epochs = 10
 weight_decay = 0.01
 MAX_LEN = 512
-sample_number = 10
+sample_number = 100
 lora_r = 32  # 16, 32
 lora_alpha = 16  # 8, 16
 lora_dropout = 0.1  # 0.05, 0.1
-sample_number = 100
 model_name = (
     # "/home/llama/Personal_Directories/srb/binary_classfication/Llama-3.2-3B-Instruct"
     "/home/snt/llm_models/Llama-3.2-1B-Instruct"
 )
-val_dataset_path = "data/fake_targets/flores_devtest_arrow"
-train_dataset_path = "data/fake_targets/NC_LUX.arrow"
-
-
-# ====================================================DATA=================================================================
-# Load validation and training datasets
-
-# val_dataset = load_from_disk("/home/llama/Personal_Directories/srb/mt_luxembourgish/data/flores_devtest_arrow").select([i for i in range(sample_number)])
-val_dataset = load_from_disk(
-    # "/home/llama/Personal_Directories/srb/mt_luxembourgish/data/flores_devtest_arrow"
-    val_dataset_path
-).rename_columns(
-    {
-        "sentence_ltz_Latn": "Luxembourgish",  # Renaming 'subsentence' to 'sentence_eng_Latn'
-        "sentence_eng_Latn": "English",  # Renaming 'translated_text' to 'sentence_ltz_Latn'
-    }
+val_dataset_path = (
+    "/home/snt/projects_lujun/mt_luxembourgish/data/fake_targets/flores_devtest_arrow"
 )
-train_dataset = (
-    load_from_disk(
-        # "/home/llama/Personal_Directories/srb/mt_luxembourgish/data/NC_LUX.arrow"
-        train_dataset_path
-    )
-    .select_columns(["subsentence", "translated_text"])
-    .rename_columns(
-        {
-            "subsentence": "Luxembourgish",  # Renaming 'subsentence' to 'sentence_eng_Latn'
-            "translated_text": "English",  # Renaming 'translated_text' to 'sentence_ltz_Latn'
-        }
-    )
+train_dataset_path = (
+    "/home/snt/projects_lujun/mt_luxembourgish/data/fake_targets/NC_LUX.arrow"
 )
-
-
-def create_prompt(sample, mode="train", src_lng="Luxembourgish", tgt_lng="English"):
-    # Define the system message template.
-    system_message = f"You are an expert {tgt_lng} translator. Translate the {src_lng} input text into {tgt_lng}.".upper()
-    input = sample[src_lng.capitalize()].strip()  # Extract the input text.
-    response = (
-        sample[tgt_lng.capitalize()].strip() if tgt_lng.capitalize() in sample else ""
-    )  # Extract the target text.
-
-    # Define the end-of-sequence token.
-    eos_token = " </s>"
-    # Construct the full prompt.
-    full_prompt = "<s> [INST] <<SYS>> " + system_message + " <</SYS>> "
-    full_prompt += input + " [/INST] "
-    if mode == "train":
-        full_prompt += response + eos_token
-    return {"prompt_response": full_prompt}
-
-
-train_dataset = (
-    train_dataset.map(
-        lambda sample: {
-            "prompt_response": create_prompt(sample, mode="train")["prompt_response"]
-        }
-    )
-    .select_columns(["prompt_response"])
-    .select([i for i in range(sample_number)])
-)
-
-val_dataset = (
-    val_dataset.map(
-        lambda sample: {
-            "prompt_response": create_prompt(sample, mode="train")["prompt_response"]
-        }
-    )
-    .select_columns(["prompt_response"])
-    .select([i for i in range(sample_number)])
-)
-
-# Convert datasets to dictionaries
-dataset = DatasetDict({"train": train_dataset, "val": val_dataset})
-
 
 # ====================================================LOAD MODEL=================================================================
 
@@ -152,18 +85,113 @@ compute_dtype = getattr(
     torch, bnb_4bit_compute_dtype
 )  # Load tokenizer and model with QLoRA configuration
 
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=use_4bit,
-    bnb_4bit_quant_type=bnb_4bit_quant_type,
-    bnb_4bit_use_double_quant=use_nested_quant,
-    bnb_4bit_compute_dtype=compute_dtype,
-)
-
+# bnb_config = BitsAndBytesConfig(
+#     load_in_4bit=use_4bit,
+#     bnb_4bit_quant_type=bnb_4bit_quant_type,
+#     bnb_4bit_use_double_quant=use_nested_quant,
+#     bnb_4bit_compute_dtype=compute_dtype,
+# )
 
 # Setting up the tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right"
+
+
+# ====================================================DATA=================================================================
+# Load validation and training datasets
+
+# val_dataset = load_from_disk("/home/llama/Personal_Directories/srb/mt_luxembourgish/data/flores_devtest_arrow").select([i for i in range(sample_number)])
+val_dataset = (
+    load_from_disk(
+        # "/home/llama/Personal_Directories/srb/mt_luxembourgish/data/flores_devtest_arrow"
+        val_dataset_path
+    )
+    .rename_columns(
+        {
+            "sentence_ltz_Latn": "Luxembourgish",  # Renaming 'subsentence' to 'sentence_eng_Latn'
+            "sentence_eng_Latn": "English",  # Renaming 'translated_text' to 'sentence_ltz_Latn'
+        }
+    )
+    .select([i for i in range(sample_number)])
+)
+train_dataset = (
+    load_from_disk(
+        # "/home/llama/Personal_Directories/srb/mt_luxembourgish/data/NC_LUX.arrow"
+        train_dataset_path
+    )
+    .select_columns(["subsentence", "translated_text"])
+    .rename_columns(
+        {
+            "subsentence": "Luxembourgish",  # Renaming 'subsentence' to 'sentence_eng_Latn'
+            "translated_text": "English",  # Renaming 'translated_text' to 'sentence_ltz_Latn'
+        }
+    )
+).select([i for i in range(sample_number)])
+
+
+def create_prompt(
+    sample, mode="train", src_lng="Luxembourgish", tgt_lng="English", tokenizer=None
+):
+    """
+    Create a prompt using the model's EOS token.
+
+    Args:
+        sample (dict): A dictionary containing source and target text.
+        mode (str): The mode, either 'train' or 'test'.
+        src_lng (str): Source language name.
+        tgt_lng (str): Target language name.
+        tokenizer: The tokenizer associated with the model (required to fetch EOS token).
+
+    Returns:
+        dict: A dictionary with the constructed prompt.
+    """
+    # Validate the tokenizer input
+    if tokenizer is None or tokenizer.eos_token is None:
+        raise ValueError("A tokenizer with a defined EOS token is required.")
+
+    # Define the system message template.
+    system_message = f"Translate the {src_lng} input text into {tgt_lng}.".upper()
+    input_text = sample[src_lng.capitalize()].strip()  # Extract the input text.
+    response = (
+        sample[tgt_lng.capitalize()].strip() if tgt_lng.capitalize() in sample else ""
+    )  # Extract the target text.
+
+    # Get the EOS token from the tokenizer.
+    eos_token = tokenizer.eos_token
+
+    # Construct the full prompt.
+    full_prompt = (
+        "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n"
+        + system_message
+        + "<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n"
+    )
+    full_prompt += (
+        input_text + "<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+    )
+    if mode == "train":
+        full_prompt += response + eos_token
+    return {"prompt_response": full_prompt}
+
+
+train_dataset = train_dataset.map(
+    lambda sample: {
+        "prompt_response": create_prompt(sample, mode="train", tokenizer=tokenizer)[
+            "prompt_response"
+        ]
+    }
+).select_columns(["prompt_response"])
+
+val_dataset = val_dataset.map(
+    lambda sample: {
+        "prompt_response": create_prompt(sample, mode="train", tokenizer=tokenizer)[
+            "prompt_response"
+        ]
+    }
+).select_columns(["prompt_response"])
+
+# Convert datasets to dictionaries
+dataset = DatasetDict({"train": train_dataset, "val": val_dataset})
 
 
 # ====================================================TRAINING=================================================================
@@ -185,8 +213,8 @@ def train_ddp_accelerate():
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
         # device_map=device_map,
-        quantization_config=bnb_config,
-        pretraining_tp=1,
+        # quantization_config=bnb_config,
+        # pretraining_tp=1,
     )
     model.config.use_cache = False
     peft_config = LoraConfig(
@@ -214,7 +242,7 @@ def train_ddp_accelerate():
             print("Your GPU supports bfloat16: accelerate training with bf16=True")
 
     # Unused TrainingArguments parameters
-    gradient_checkpointing = True  # Enable gradient checkpointing 节省内存
+    # gradient_checkpointing = True  # Enable gradient checkpointing 节省内存
 
     # Hyper Parameters
     training_arguments = TrainingArguments(
@@ -243,31 +271,16 @@ def train_ddp_accelerate():
         # load_best_model_at_end=True,
         # metric_for_best_model="accuracy",
         # greater_is_better=True
+        ddp_find_unused_parameters=False,
+        disable_tqdm=False,
     )
 
     model = prepare_model_for_kbit_training(model)
     model = get_peft_model(model, peft_config)
 
     # printing before lora
-    print_trainable_parameters(model)
+    print(print_trainable_parameters(model))
     writer = SummaryWriter(log_dir=output_dir)
-
-    class CustomSFTTrainer(SFTTrainer):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            # Initialize TensorBoard writer
-            log_dir = kwargs.get("args").output_dir  # Use the training output directory
-            self.writer = writer
-
-        def log(self, logs):
-            """
-            Override the default log method to add custom TensorBoard logging.
-            """
-            super().log(logs)  # Call the parent class's log method
-            if self.state.global_step is not None:
-                for key, value in logs.items():
-                    if isinstance(value, (int, float)):
-                        self.writer.add_scalar(key, value, self.state.global_step)
 
     # # Define the early stopping callback
     # early_stopping = EarlyStoppingCallback(
@@ -303,7 +316,7 @@ def train_ddp_accelerate():
     )
 
     # Train the model
-    trainer.train()
+    trainer.train(resume_from_checkpoint=resume_from_checkpoint)
 
 
 from accelerate import Accelerator
